ea5e868a502f6b45bc77d1c40aa16d26b5450a9c
Dmitry <cher-di@mail.ru>
2020-09-13 13:11:15 +0300

    Added Windows support for discovering bluetooth devices

--- src/bluetooth.py
+++ src/bluetooth.py
@@ -1,10 +1,20 @@
 import subprocess
 import re
+import platform
 
 MAC_REGEXP = re.compile('([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})')
 
 
 def scan():
+    if platform.system() == 'Windows':
+        return scan_windows()
+    elif platform.system() == 'Linux':
+        return scan_linux()
+    else:
+        raise Exception(f'Unsupported OS: {platform.system()}')
+
+
+def scan_linux():
     result = subprocess.run(['hcitool', 'scan'],
                             capture_output=True,
                             text=True,
@@ -17,5 +27,22 @@ def scan():
         mac = re.search(MAC_REGEXP, line).group()
         name = line.replace(mac, '').replace('\t', '')
         addrs[mac] = name
+
+    return addrs
+
+
+def scan_windows():
+    result = subprocess.run(['btdiscovery', '-d', '%a%\\t%n%\\t%r%'],
+                            capture_output=True,
+                            text=True,
+                            check=True)
+
+    addrs = dict()
+    for line in result.stdout.split('\n')[:-1]:
+        # skip last string because it is empty
+        mac, name, remembered = line.split('\t')
+        mac = mac[1:-1]  # delete brackets
+        if remembered == 'No':
+            addrs[mac] = name
 
     return addrs
